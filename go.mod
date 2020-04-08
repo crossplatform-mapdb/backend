7e0f5c5dceba181ec1f1e93871f0ef89b8bbab2b
zack <zackmyers@lavabit.com>
2020-04-07 23:59:47 -1000

    Add Login(kinda, no tokens yet)

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.3.2
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -18,18 +19,32 @@ import (
 // User defines the structure of a User
 type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Username string             `json:"username,omitempty bson:"username,omitempty`
-	Email    string             `json:"email,omitempty bson:"email,omitempty`
-	Password string             `json:"password,omitempty bson:"password,omitempty`
+	Username string             `json:"username,omitempty" bson:"username,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
 }
 
 var client *mongo.Client
+
+// HashPassword hashes the password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash checks to see if the password is correct
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
 // CreateUserEndpoint Create a new User
 func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	var user User
 	json.NewDecoder(request.Body).Decode(&user)
+	user.Password, _ = HashPassword(user.Password)
+	fmt.Println(user.Password)
 	collection := client.Database("mapdb").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	result, _ := collection.InsertOne(ctx, user)
@@ -79,6 +94,31 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(user)
 }
 
+// LoginEndpoint checks to see if a user is who they claim to be and sends them a JWT.
+func LoginEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	username := params["username"]
+	var loginUser User
+	var dbUser User
+	json.NewDecoder(request.Body).Decode(&loginUser)
+	collection := client.Database("mapdb").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{Username: username}).Decode(&dbUser)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	match := CheckPasswordHash(loginUser.Password, dbUser.Password)
+	if match == true {
+		response.Write([]byte(`{ "message": "<token>" }`))
+	} else {
+		response.WriteHeader(http.StatusUnauthorized)
+		response.Write([]byte(`{ "message": "you could not be logged in." }`))
+	}
+}
+
 func main() {
 	fmt.Println("Starting the application...")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
@@ -87,5 +127,6 @@ func main() {
 	router.HandleFunc("/api/signup", CreateUserEndpoint).Methods("POST")
 	router.HandleFunc("/api/user/{id}", GetUserEndpoint).Methods("GET")
 	router.HandleFunc("/api/users", GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/api/login", LoginEndpoint).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
