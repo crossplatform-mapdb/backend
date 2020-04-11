f16bdd55f50b0ce505549a01e201d13551b0f215
zack <zackmyers@lavabit.com>
2020-04-11 11:39:28 -1000

    Allow you to view your own email and password, but not other users.

--- login.go
+++ login.go
@@ -29,27 +29,15 @@ func CheckPasswordHash(password, hash string) bool {
 func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	var signUpUser User
-	var dbUser User
-	var db2User User
 	json.NewDecoder(request.Body).Decode(&signUpUser)
 	collection := client.Database("mapdb").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	dbUser, err := GetUserByUsername(signUpUser.Username)
-	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(` { "message": "` + err.Error() + `"}`))
-		return
-	}
+	dbUser, _ := GetUserByUsername(signUpUser.Username)
 	if dbUser.Username == signUpUser.Username {
 		response.Write([]byte(`{ "message": "` + "An account has already been created with that username, please choose another." + `" }`))
 		return
 	}
-	db2User, err2 := GetUserByEmail(signUpUser.Email)
-	if err2 != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(` { "message": "` + err2.Error() + `"}`))
-		return
-	}
+	db2User, _ := GetUserByEmail(signUpUser.Email)
 	if db2User.Email == signUpUser.Email {
 		response.Write([]byte(`{ "message": "` + "An account has already been created with that email, try logging in." + `" }`))
 		return
@@ -83,16 +71,18 @@ func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
 	for cursor.Next(ctx) {
 		var user User
 		cursor.Decode(&user)
-		user2, err := GetUserByUsername(GetUsernameFromToken(token.Token))
 		if err != nil {
 			response.WriteHeader(http.StatusInternalServerError)
 			response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
 			return
 		}
-		admin, _ := strconv.ParseBool(user2.Admin)
-		if admin == false {
-			user.Password = "hidden"
-			user.Email = "hidden"
+		admin, _ := GetUserByUsername(GetUsernameFromToken(token.Token))
+		adminCheck, _ := strconv.ParseBool(admin.Admin)
+		if adminCheck == false {
+			if admin.Username != user.Username {
+				user.Password = "hidden"
+				user.Email = "hidden"
+			}
 		}
 		users = append(users, user)
 	}
@@ -116,7 +106,6 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	params := mux.Vars(request)
 	id, _ := primitive.ObjectIDFromHex(params["id"])
-	var user User
 	user, err := GetUserByID(id)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
@@ -126,8 +115,10 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	admin, _ := GetUserByUsername(GetUsernameFromToken(token.Token))
 	adminCheck, _ := strconv.ParseBool(admin.Admin)
 	if adminCheck == false {
-		user.Password = "hidden"
-		user.Email = "hidden"
+		if admin.Username != user.Username {
+			user.Password = "hidden"
+			user.Email = "hidden"
+		}
 	}
 	json.NewEncoder(response).Encode(user)
 }
@@ -136,7 +127,6 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 func LoginEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var loginUser User
-	var dbUser User
 	json.NewDecoder(request.Body).Decode(&loginUser)
 	dbUser, err := GetUserByUsername(loginUser.Username)
 	if err != nil {
